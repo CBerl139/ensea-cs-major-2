bc2420839891132175218d4c7a30b09826325af4
ensea <ensea@StudentLab>
2023-12-18 11:37:15 +0100

    read request successfully sent

--- src/gettftp.c
+++ src/gettftp.c
@@ -19,6 +19,20 @@ int displayIPaddress(struct addrinfo * res){
 	return EXIT_SUCCESS;	
 }
 
+int createRRQmsg(char * file, char * mode, char * buffer){
+	int file_size = strlen(file);
+	int msg_size = 2 + strlen(file) + 1 +  strlen(mode) + 1;	
+	
+	buffer[0] = 0;
+	buffer[1] = 1;
+	strcpy(buffer + 2, file);	
+	buffer[2 + file_size] = 0;
+	strcpy(buffer + 2 + file_size + 1, mode);	
+	buffer[msg_size - 1] = 0;	
+	
+	return EXIT_SUCCESS;
+}
+
 int main (int argc, char ** argv){
 	if (argc != 4){
 		printf("usage : ./gettftp <host> <port> <file>\n");
@@ -26,7 +40,7 @@ int main (int argc, char ** argv){
 	}
 
 	struct addrinfo hints;
-	// asign all values of hints to 0
+	// asign all values of hints to 0  (except for family and socktype)
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_DGRAM;
@@ -41,9 +55,22 @@ int main (int argc, char ** argv){
 	printf("file : %s\n",argv[3]);	
 	
 	int socket_fd;
-	if (socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)){
-		printf("socket_fd = %d\n",socket_fd);
+	char mode[] = "octet";
+	int msg_size = 2 + strlen(argv[3]) + 1 +  strlen(mode) + 1;
+	char RRQ_msg[msg_size];
+	
+	createRRQmsg(argv[3], mode, RRQ_msg);
+
+	if ((socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1){
+		perror("socket failure");
+		return 1;
 	}
+
+	printf("socket_fd = %d\n",socket_fd);
+	int sendtoReturn = sendto(socket_fd,RRQ_msg,msg_size,0,res->ai_addr,res->ai_addrlen);
+	perror("sendto");
+	printf("sendto = %d\n",sendtoReturn);
+	
 	
 	return EXIT_SUCCESS;
 }
